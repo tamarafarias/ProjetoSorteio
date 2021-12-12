5efeab6b68fb6c2d84b6d18f4a48545543c70664
tamarafarias <tfarias37@gmail.com>
2021-12-12 16:37:42 -0300

    Inicia o Projeto constraint layout

---
+++ SegundoProjeto/app/src/main/java/com/example/segundoprojeto/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.segundoprojeto;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @java.lang.Override
+    protected void onCreate(@Nullable android.os.Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
